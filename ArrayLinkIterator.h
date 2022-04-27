31baa71bf9bd62d78691b118aa21980c50d9bc3a
Dom Silva <silvadom2014@gmail.com>
2022-04-27 22:16:27 +0400

    WIP iterator class

--- ArrayLinkIterator.h
+++ ArrayLinkIterator.h
@@ -1,9 +1,27 @@
 
 template<typename N>
-class ArrayLinkIterator
+struct ArrayLinkIterator
 {
-public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type   = std::ptrdiff_t;
+    using value_type        = N;
+    using pointer           = N*;
+    using reference         = N&;
 
+    ArrayLinkIterator(pointer ptr) : m_ptr(ptr) 
+    {
+        array = (m_ptr == nullptr) ? m_ptr: m_ptr[0]; 
+    }
+    ArrayLinkIterator(const ArrayLinkIterator& other) : m_ptr(other.mptr) {}
+
+    reference operator*() const {return *m_ptr; }
+    pointer operator->() const {return m_ptr; }
+
+    ArrayLinkIterator& operator++() {
+        m_ptr = (m_ptr->next != -1) ? array + m_ptr->next : nullptr;
+        return *this;
+    }
 private:
+    pointer m_ptr;
     N* array;
 };
